6ed21ca5decb88f03a8fbe11c5c2f53219994873
Vatan Prakash <vatanprakash867@gmail.com>
2024-05-17 19:36:33 +0530

    Fixed issue

--- src/app/page.js
+++ src/app/page.js
@@ -1,123 +1,4 @@
-// import Image from "next/image";
-
-// export default function Home() {
-//   return (
-//     <>
-//     <div className="bg-[#242424] px-6 ">
-//       <div className="h-[78px] bg-[#0E77D9] mt-[40px] mb-[20px] pt-2 rounded-[12px] border-[1px]">
-//         <div className="">
-//             <Image src=''/>
-//         </div>
-//         <div className="">
-//         Your daily goal almost done
-//         </div>
-//       </div>
-
-//         <div className="mid w-full h-[475px] bg-[#242424]   ">
-//           <div>
-//             <div className="flex justify-between h-[28px] py-1">
-//               <div className="text-[17px] text-[white]">Today's Goal</div>
-//               <div>
-//                 <Image src="/./heart.svg" />
-//               </div>
-//             </div>
-
-//             <div className="h-[365px] gap-[2px] mt-2">
-//               <div className="h-[65px] mt-2 p-2 pt-4 rounded-[12px] border-[1px] border-[#fffffff6]">
-//                 <div className="flex justify-between w-full  h-[32px]">
-//                   <div className="flex justify-between gap-3">
-//                     <Image src="/work.svg" />
-//                     <div className="text-[16px] text mt-1 text-[white]">
-//                       Workout for 20 Mins
-//                     </div>
-//                   </div>
-//                   <div>
-//                     <Image src="/wdonetick.svg" />
-//                   </div>
-//                 </div>
-//               </div>
-
-//               <div className="h-[65px] mt-2 p-2 pt-4 rounded-[12px] border-[1px] border-[#fffffff6]">
-//                 <div className="flex justify-between w-full  h-[32px]">
-//                   <div className="flex justify-between gap-3">
-//                     <Image src="/readbook.svg" />
-//                     <div className="text-[16px] text mt-1 text-[white]">
-//                     Read book for 15 mins
-//                     </div>
-//                   </div>
-//                   <div>
-//                     <Image src="/readbooktick.svg" />
-//                   </div>
-//                 </div>
-//               </div>
-
-//               <div className="h-[65px] mt-2 p-2 pt-4 rounded-[12px] border-[1px] border-[#fffffff6]">
-//                 <div className="flex justify-between w-full  h-[32px]">
-//                   <div className="flex justify-between gap-3">
-//                     <Image src="/walk.svg" />
-//                     <div className="text-[16px] text mt-1 text-[white]">
-//                     30 mins walk
-//                     </div>
-//                   </div>
-//                   <div>
-//                     <Image src="/walktick.svg" />
-//                   </div>
-//                 </div>
-//               </div>
-
-//               <div className="h-[65px] mt-2 p-2 pt-4 rounded-[12px] border-[1px] border-[#fffffff6]">
-//                 <div className="flex justify-between w-full  h-[32px]">
-//                   <div className="flex justify-between gap-3">
-//                     <Image src="/sleep.svg" />
-//                     <div className="text-[16px] text mt-1 text-[white]">
-//                     Sleep at 11 sharp
-//                     </div>
-//                   </div>
-//                   <div>
-//                     <Image src="/sleeptick.svg" />
-//                   </div>
-//                 </div>
-//               </div>
-
-//               <div className="h-[65px] mt-2 p-2 pt-4 rounded-[12px] border-[1px] border-[#fffffff6]">
-//                 <div className="flex justify-between w-full  h-[32px]">
-//                   <div className="flex justify-between gap-3">
-//                     <Image src="/drink.svg" />
-//                     <div className="text-[16px] text mt-1 text-[white]">
-//                     Drink 3L water
-//                     </div>
-//                   </div>
-//                   <div>
-//                     <Image src="/drinktick.svg" />
-//                   </div>
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-
-
-//           {/* <div className="flex justify-between gap-[50px] upper w-full h-[50px] text-[white] bg-[#D15439] mb-[40px] p-2 pt-4 rounded-[38.03px] border-[1px] border-[#fffffff6]">
-//               <div className="w-8 h-8 rounded-full bg-gray-400 ">
-                
-//               </div>
-//               <div className="text-xl font-medium ">
-//                   Swipe to track all
-//               </div> 
-//           </div> */}
-//   <div className="flex justify-between gap-[50px] upper w-full h-[50px] text-[white] bg-[#D15439] mb-[40px] p-2 pt-4 rounded-[38.03px] border-[1px] border-[#fffffff6]">
-//   <div className="circle flex items-center justify-center bg-white rounded-full w-8 h-8"></div>
-//   <div className="text-container flex items-center justify-center text-xl font-medium">
-//     Swipe to track all
-//   </div>
-// </div>
-//         </div>
-
-       
-//       </div>
-//     </>
-//   );
-// }
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -128,7 +9,6 @@ export default function Home() {
             <div className="w-[32px] h-[32px] bg-[#fff] rounded-full flex justify-center items-center">
               <div className="w-[20px] h-[20px] bg-[#7DA7CE] rounded-full flex justify-center items-center">
                 <div className="w-[12px] h-[12px] bg-[#fff] rounded-full flex justify-center items-center">
-
                 </div>
               </div>
             </div>
@@ -142,12 +22,12 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="mid w-full h-[475px] bg-[#242424]   ">
+        <div className="mid w-full h-[475px] bg-[#242424]">
           <div>
             <div className="flex justify-between h-[28px] py-1">
               <div className="text-[17px] text-[white]">Today's Goal</div>
               <div>
-                <Image src="/./heart.svg" alt="Page  Icon"/>
+                <Image src="/heart.svg" alt="Heart Icon" width={24} height={24} />
               </div>
             </div>
 
@@ -155,13 +35,13 @@ export default function Home() {
               <div className="h-[65px] mt-2 p-2 pt-4 rounded-[12px] border-[1px] border-[#fffffff6]">
                 <div className="flex justify-between w-full  h-[32px]">
                   <div className="flex justify-between gap-3">
-                    <Image src="/work.svg" alt="Page  Icon"/>
+                    <Image src="/work.svg" alt="Work Icon" width={24} height={24} />
                     <div className="text-[16px] text mt-1 text-[white]">
                       Workout for 20 Mins
                     </div>
                   </div>
                   <div>
-                    <Image src="/wdonetick.svg " alt="Page  Icon" />
+                    <Image src="/wdonetick.svg" alt="Work Done Icon" width={24} height={24} />
                   </div>
                 </div>
               </div>
@@ -169,13 +49,13 @@ export default function Home() {
               <div className="h-[65px] mt-2 p-2 pt-4 rounded-[12px] border-[1px] border-[#fffffff6]">
                 <div className="flex justify-between w-full  h-[32px]">
                   <div className="flex justify-between gap-3">
-                    <Image src="/readbook.svg" alt="Page  Icon"/>
+                    <Image src="/readbook.svg" alt="Read Book Icon" width={24} height={24} />
                     <div className="text-[16px] text mt-1 text-[white]">
                       Read book for 15 mins
                     </div>
                   </div>
                   <div>
-                    <Image src="/readbooktick.svg" alt="Page  Icon"/>
+                    <Image src="/readbooktick.svg" alt="Read Book Done Icon" width={24} height={24} />
                   </div>
                 </div>
               </div>
@@ -183,13 +63,13 @@ export default function Home() {
               <div className="h-[65px] mt-2 p-2 pt-4 rounded-[12px] border-[1px] border-[#fffffff6]">
                 <div className="flex justify-between w-full  h-[32px]">
                   <div className="flex justify-between gap-3">
-                    <Image src="/walk.svg" alt="Page  Icon"/>
+                    <Image src="/walk.svg" alt="Walk Icon" width={24} height={24} />
                     <div className="text-[16px] text mt-1 text-[white]">
                       30 mins walk
                     </div>
                   </div>
                   <div>
-                    <Image src="/walktick.svg" alt="Page  Icon"/>
+                    <Image src="/walktick.svg" alt="Walk Done Icon" width={24} height={24} />
                   </div>
                 </div>
               </div>
@@ -197,13 +77,13 @@ export default function Home() {
               <div className="h-[65px] mt-2 p-2 pt-4 rounded-[12px] border-[1px] border-[#fffffff6]">
                 <div className="flex justify-between w-full  h-[32px]">
                   <div className="flex justify-between gap-3">
-                    <Image src="/sleep.svg" alt="Page  Icon"/>
+                    <Image src="/sleep.svg" alt="Sleep Icon" width={24} height={24} />
                     <div className="text-[16px] text mt-1 text-[white]">
                       Sleep at 11 sharp
                     </div>
                   </div>
                   <div>
-                    <Image src="/sleeptick.svg" alt="Page  Icon"/>
+                    <Image src="/sleeptick.svg" alt="Sleep Done Icon" width={24} height={24} />
                   </div>
                 </div>
               </div>
@@ -211,28 +91,19 @@ export default function Home() {
               <div className="h-[65px] mt-2 p-2 pt-4 rounded-[12px] border-[1px] border-[#fffffff6]">
                 <div className="flex justify-between w-full  h-[32px]">
                   <div className="flex justify-between gap-3">
-                    <Image src="/drink.svg" alt="Page  Icon"/>
+                    <Image src="/drink.svg" alt="Drink Icon" width={24} height={24} />
                     <div className="text-[16px] text mt-1 text-[white]">
                       Drink 3L water
                     </div>
                   </div>
                   <div>
-                    <Image src="/drinktick.svg" alt="Page  Icon"/>
+                    <Image src="/drinktick.svg" alt="Drink Done Icon" width={24} height={24} />
                   </div>
                 </div>
               </div>
             </div>
           </div>
 
-
-          {/* <div className="flex justify-between gap-[50px] upper w-full h-[50px] text-[white] bg-[#D15439] mb-[40px] p-2 pt-4 rounded-[38.03px] border-[1px] border-[#fffffff6]">
-              <div className="w-8 h-8 rounded-full bg-gray-400 ">
-                
-              </div>
-              <div className="text-xl font-medium ">
-                  Swipe to track all
-              </div> 
-          </div> */}
           <div className="flex justify-between items-center px-4 w-full h-[50px] text-[white] bg-[#D15439] mb-[40px] rounded-[38.03px] border-[1px] border-[#fffffff6]">
             <div className="circle flex items-center justify-center bg-white rounded-full w-8 h-8"></div>
             <div className="text-container flex items-center justify-center text-xl font-medium">
@@ -240,8 +111,6 @@ export default function Home() {
             </div>
           </div>
         </div>
-
-
       </div>
     </>
   );
--- src/components/StickyBar.jsx
+++ src/components/StickyBar.jsx
@@ -13,28 +13,29 @@ const StickyBar = () => {
 
         <Link href="/">
           <div className='flex flex-col items-center gap-[2px]'>
-            <Image src={pathname === '/' ? "/firered.svg" : "/fire.svg"} className='w-[40px] h-[40px]' alt="Icon"/>
+          <Image src={pathname === '/' ? "/firered.svg" : "/fire.svg"} width={40} height={40} className='w-[40px] h-[40px]' alt="Icon"/>
+
             <div className={`text-[10px] ${pathname === '/' ? "text-[#D15439]" : "text-[#B5B5B5]"}`}>Page1</div>
           </div>
         </Link>
 
         <Link href="/page2">
           <div className='flex flex-col items-center gap-[2px]'>
-            <Image src={pathname === '/page2' ? "/trainingred.svg" : "/training.svg"} className='w-[40px] h-[40px]' alt="Icon"/>
+            <Image src={pathname === '/page2' ? "/trainingred.svg" : "/training.svg"} width={40} height={40}  className='w-[40px] h-[40px]' alt="Icon"/>
             <div className={`text-[10px] ${pathname === '/page2' ? "text-[#D15439]" : "text-[#B5B5B5]"}`} >Page2</div>
           </div>
         </Link>
 
         <Link href="/page3">
           <div className='flex flex-col items-center gap-[2px]'>
-            <Image src={pathname === '/page3' ? "/foodred.svg" : "/food.svg"} className='w-[40px] h-[40px]'alt="Icon" />
+            <Image src={pathname === '/page3' ? "/foodred.svg" : "/food.svg"}  width={40} height={40}  className='w-[40px] h-[40px]'alt="Icon" />
             <div className={`text-[10px] ${pathname === '/page3' ? "text-[#D15439]" : "text-[#B5B5B5]"}`}>Page3</div>
           </div>
         </Link>
 
         <Link href="/page4">
           <div className='flex flex-col items-center gap-[2px]'>
-            <Image src={pathname === '/page4' ? "/progressred.svg" : "/progress.svg"} className='w-[40px] h-[40px]' alt="Icon" />
+            <Image src={pathname === '/page4' ? "/progressred.svg" : "/progress.svg"}  width={40} height={40}   className='w-[40px] h-[40px]' alt="Icon" />
             <div className={`text-[10px] ${pathname === '/page4' ? "text-[#D15439]" : "text-[#B5B5B5]"}`}>Page4</div>
           </div>
         </Link>
